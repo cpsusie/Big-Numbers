4559e13d2a3503af63f98c40a8f7a47d474e63be
Jesper <jesper.gr.mikkelsen@gmail.com>
2016-07-11 17:55:02 +0200

    Nyt versionsnummer på Colormap-controllen, så vi kan køremed den gamle også

--- Lib/Src/Colormap/Colormap.cpp
+++ Lib/Src/Colormap/Colormap.cpp
@@ -12,65 +12,46 @@ CColormapApp theApp;
 
 const GUID CDECL _tlid = { 0x60305F72, 0xC994, 0x4F73, { 0xB8, 0xC3, 0xD3, 0x6A, 0x9D, 0x54, 0x3C, 0x67 } };
 const WORD _wVerMajor = 1;
-const WORD _wVerMinor = 0;
-
-
+const WORD _wVerMinor = 1;
 
 // CColormapApp::InitInstance - DLL initialization
-
-BOOL CColormapApp::InitInstance()
-{
+BOOL CColormapApp::InitInstance() {
 	BOOL bInit = COleControlModule::InitInstance();
 
-	if (bInit)
-	{
+	if (bInit) {
 		// TODO: Add your own module initialization code here.
 	}
-
 	return bInit;
 }
 
-
-
 // CColormapApp::ExitInstance - DLL termination
-
-int CColormapApp::ExitInstance()
-{
+int CColormapApp::ExitInstance() {
 	// TODO: Add your own module termination code here.
-
 	return COleControlModule::ExitInstance();
 }
 
-
-
 // DllRegisterServer - Adds entries to the system registry
-
-STDAPI DllRegisterServer(void)
-{
+STDAPI DllRegisterServer() {
 	AFX_MANAGE_STATE(_afxModuleAddrThis);
 
-	if (!AfxOleRegisterTypeLib(AfxGetInstanceHandle(), _tlid))
-		return ResultFromScode(SELFREG_E_TYPELIB);
-
-	if (!COleObjectFactoryEx::UpdateRegistryAll(TRUE))
-		return ResultFromScode(SELFREG_E_CLASS);
-
+  if (!AfxOleRegisterTypeLib(AfxGetInstanceHandle(), _tlid)) {
+    return ResultFromScode(SELFREG_E_TYPELIB);
+  }
+  if (!COleObjectFactoryEx::UpdateRegistryAll(TRUE)) {
+    return ResultFromScode(SELFREG_E_CLASS);
+  }
 	return NOERROR;
 }
 
-
-
 // DllUnregisterServer - Removes entries from the system registry
-
-STDAPI DllUnregisterServer(void)
-{
+STDAPI DllUnregisterServer() {
 	AFX_MANAGE_STATE(_afxModuleAddrThis);
 
-	if (!AfxOleUnregisterTypeLib(_tlid, _wVerMajor, _wVerMinor))
-		return ResultFromScode(SELFREG_E_TYPELIB);
-
-	if (!COleObjectFactoryEx::UpdateRegistryAll(FALSE))
-		return ResultFromScode(SELFREG_E_CLASS);
-
+  if (!AfxOleUnregisterTypeLib(_tlid, _wVerMajor, _wVerMinor)) {
+    return ResultFromScode(SELFREG_E_TYPELIB);
+  }
+  if (!COleObjectFactoryEx::UpdateRegistryAll(FALSE)) {
+    return ResultFromScode(SELFREG_E_CLASS);
+  }
 	return NOERROR;
 }
