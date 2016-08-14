0098c94c5c491ff20601340b9408fbf7f4c3257f
Jesper <jesper.gr.mikkelsen@gmail.com>
2016-08-14 23:58:21 +0200

    Tilføjet 3 function til PropertyContainer:setBoolProperty, setIntProperty og setStringProperty

--- Lib/Include/PropertyChangeListener.h
+++ Lib/Include/PropertyChangeListener.h
@@ -14,6 +14,10 @@ private:
   CompactArray<PropertyChangeListener*> m_listeners;
 protected:
   void notifyPropertyChanged(int id, const void *oldValue, const void *newValue) const;
+  void setBoolProperty(  int id, bool &v, bool newValue);
+  void setIntProperty(   int id, int  &v, int  newValue);
+  void setStringProperty(int id, String  &v, const String  &newValue);
+
 public:
   void addPropertyChangeListener(   PropertyChangeListener *listener, bool first = false);
   void removePropertyChangeListener(PropertyChangeListener *listener);
--- Lib/Src/Util/PropertyContainer.cpp
+++ Lib/Src/Util/PropertyContainer.cpp
@@ -7,11 +7,35 @@ void PropertyContainer::notifyPropertyChanged(int id, const void *oldValue, cons
   }
 }
 
+void PropertyContainer::setBoolProperty(int id, bool &v, bool newValue) {
+  if(newValue != v) {
+    const bool oldValue = v;
+    v = newValue;
+    notifyPropertyChanged(id, &oldValue, &v);
+  }
+}
+
+void PropertyContainer::setIntProperty(int id, int &v, int newValue) {
+  if(newValue != v) {
+    const int oldValue = v;
+    v = newValue;
+    notifyPropertyChanged(id, &oldValue, &v);
+  }
+}
+
 void PropertyContainer::addPropertyChangeListener(PropertyChangeListener *listener, bool first) {
   if(first) {
     m_listeners.add(0, listener);
   } else {
     m_listeners.add(listener);
+  }
+}
+
+void PropertyContainer::setStringProperty(int id, String  &v, const String  &newValue) {
+  if (newValue != v) {
+    const String old = v;
+    v = newValue;
+    notifyPropertyChanged(id, &old, &v);
   }
 }
 
