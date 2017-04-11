8515212fe3986c97c3376d6970d380f7f7d8c984
Jesper <jesper.gr.mikkelsen@gmail.com>
2017-04-11 12:13:00 +0200

    sat bedre kommentarer på Sperical-klassen

--- Lib/Include/Math/Spherical.h
+++ Lib/Include/Math/Spherical.h
@@ -6,8 +6,10 @@ class Spherical {
 protected:
   void init(double x, double y, double z);
 public:
-  double r, fi, theta; // r distance to (0,0,0), fi angle between z-axis and (x,y,z), theta angle between x-axis and (x,y,z)
-                       // 0 <= r, 0 <= fi <= pi, 0 <= theta <= 2pi. theta > pi >= y < 0
+  double r;     // Distance to (0,0,0), 0 <= r
+  double fi;    // Angle between z-axis and (x,y,z), 0 <= fi <= pi
+  double theta; // Angle between x-axis and (x,y,z), 0 <= theta <= 2pi
+                // theta > pi => y < 0
   Spherical();
   Spherical(const Point3D &p);
   Spherical(double x, double y, double z);
