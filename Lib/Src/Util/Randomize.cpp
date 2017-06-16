ffb702613ec1ff9c412cf8f5cfb6b45332bf60ec
Jesper <jesper.gr.mikkelsen@gmail.com>
2017-06-16 09:01:18 +0200

    indsat const og omdøbt __int64 -> INT64

--- Lib/Src/Util/Randomize.cpp
+++ Lib/Src/Util/Randomize.cpp
@@ -5,20 +5,20 @@
 #pragma comment(lib, "ole32.lib")
 
 void randomize() {
-  const __int64 seed = getRandomSeed();
+  const INT64 seed = getRandomSeed();
   srand((UINT)seed);
   _standardRandomGenerator.setSeed(seed);
 }
 
-__int64 getRandomSeed() {
+INT64 getRandomSeed() {
   GUID guid;
   CoCreateGuid(&guid);
-  short w4 = ((short)guid.Data4[0] << 8) | guid.Data4[1];
-  short w5 = ((short)guid.Data4[2] << 8) | guid.Data4[3];
-  short w6 = ((short)guid.Data4[4] << 8) | guid.Data4[5];
-  short w7 = ((short)guid.Data4[6] << 8) | guid.Data4[7];
+  const short w4 = ((short)guid.Data4[0] << 8) | guid.Data4[1];
+  const short w5 = ((short)guid.Data4[2] << 8) | guid.Data4[3];
+  const short w6 = ((short)guid.Data4[4] << 8) | guid.Data4[5];
+  const short w7 = ((short)guid.Data4[6] << 8) | guid.Data4[7];
 
-  __int64 seed = ((((((__int64)guid.Data1 * 151i64
+  INT64 seed = ((((((INT64)guid.Data1 * 151i64
                 + guid.Data2) * 641
                 + guid.Data3) * 983
                 + w4        ) * 337
