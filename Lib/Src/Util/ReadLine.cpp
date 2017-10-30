38cfd5ca8a013f4c4837d5fcbb5547b35ce85a81
Jesper <jesper.gr.mikkelsen@gmail.com>
2017-10-31 00:56:06 +0100

    mindre optimering af readLine (resetter ikke str fra start)

--- Lib/Src/Util/ReadLine.cpp
+++ Lib/Src/Util/ReadLine.cpp
@@ -2,11 +2,14 @@
 
 bool readLine(FILE *f, String &str) {
   TCHAR line[4096];
-  str = EMPTYSTRING;
   bool result = false;
   while(_fgetts(line, ARRAYSIZE(line), f)) {
+    if(result) {
+      str += line;
+    } else {
+      str = line;
+    }
     result = true;
-    str += line;
     size_t len = str.length();
     if((len > 0) && str.last() == _T('\n')) {
       str.removeLast(); len--;
