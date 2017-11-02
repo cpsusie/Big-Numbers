49550c9b9f4003e7bb386a4b83136454aa8aa0a9
Jesper <jesper.gr.mikkelsen@gmail.com>
2017-11-03 00:40:05 +0100

    Forbedret visningen af Double80, så den ligner double's debugtext

--- Lib/Src/NumberAddin/Double80Addin.cpp
+++ Lib/Src/NumberAddin/Double80Addin.cpp
@@ -6,11 +6,17 @@ ADDIN_API HRESULT WINAPI AddIn_Double80(DWORD dwAddress, DEBUGHELPER *pHelper, i
     Double80 d;
     pHelper->getRealObject(&d, sizeof(d));
     char str[150];
-    Double80::d80toa(str, d);
+    const int expo10 = Double80::getExpo10(d);
+    if(expo10 < -4 || (expo10 >= 18)) {
+      Double80::d80toa(str, d);
+    } else {
+      String s = toString(d, 17-expo10,19,ios::fixed);
+      USES_CONVERSION;
+      strcpy(str,T2A(s.cstr()));
+    }
     strncpy(pResult, str, maxResult);
-  }
-  catch (...) {
-    strncpy(pResult, "Invalid address", maxResult);
+  } catch (...) {
+    strncpy(pResult, "", maxResult);
   }
   return S_OK;
 }
