3484c81dfcb20b8f49b816ec554ccf059759b6ba
Jesper <jesper.gr.mikkelsen@gmail.com>
2017-12-22 20:39:12 +0100

    Tilføjet kopi af funktionen D80ToDbgString, der anvendes i NumberAddIn til visning af Double80 i Debuggeren

--- Test/UnitTest/TestToString/main.cpp
+++ Test/UnitTest/TestToString/main.cpp
@@ -1,10 +1,25 @@
 #include "stdafx.h"
+#include <Math/Double80.h>
 #include "TestToString.h"
 
 using namespace std;
 
+String D80ToDbgString(const Double80 &d80) {
+  const int expo10 = Double80::getExpo10(d80);
+  if(expo10 < -4 || (expo10 >= 18)) {
+    TCHAR str[50];
+    return d80tot(str, d80);
+  } else {
+    return toString(d80, 19-expo10,21,ios::fixed);
+  }
+}
+
+
 int main(int argc, char **argv) {
   try {
+    const UINT64   ui64 = -1;
+    const Double80 x    = ui64;
+    const String   s1   = D80ToDbgString(x);
     testToString();
   } catch(Exception e) {
     tcout << _T("Exception:") << e.what() << endl;
