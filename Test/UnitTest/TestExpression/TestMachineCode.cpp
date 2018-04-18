4e1fe58555a6d50f906732c15f7644820a429348
Jesper <jesper.gr.mikkelsen@gmail.com>
2018-04-19 00:30:11 +0200

    removed clear-statement

--- Test/UnitTest/TestExpression/TestMachineCode.cpp
+++ Test/UnitTest/TestExpression/TestMachineCode.cpp
@@ -799,12 +799,10 @@ void TestMachineCode::testBitOperations() {
   testOpcode(BSF    );
   testOpcode(BSR    );
 
-  clear(true);
   testOpcode(BT     );
   testOpcode(BTS    );
   testOpcode(BTR    );
   testOpcode(BTC    );
-
 }
 
 void TestMachineCode::testStringInstructions() {
