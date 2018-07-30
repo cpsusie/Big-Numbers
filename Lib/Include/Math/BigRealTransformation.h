ff329880ffe6c0a9539c844d359e2ce11f57262a
Jesper <jesper.gr.mikkelsen@gmail.com>
2018-07-30 02:36:28 +0200

    First version of BigRealIntervalTransformation and BigRealRectangleTransformation with AUTOPRECISION (Not testet)

--- Lib/Include/Math/BigRealTransformation.h
+++ Lib/Include/Math/BigRealTransformation.h
@@ -3,100 +3,57 @@
 #include "Transformation.h"
 #include "BigRealInterval.h"
 
+#define AUTOPRECISION 0
+
 class BigRealIntervalTransformation {
-private:
-  DECLARECLASSNAME;
 private:
   BigRealInterval m_fromInterval, m_toInterval;
   BigReal         m_a, m_b;
+  UINT            m_precision, m_digits;
 protected:
-  UINT m_digits;
-  void checkPrecision(UINT digits);
   virtual BigReal translate(       const BigReal &x) const = 0;
   virtual BigReal inverseTranslate(const BigReal &x) const = 0;
-  void computeTransformation() {
-    const BigRealInterval &toInterval   = getToInterval();
-    const BigRealInterval &fromInterval = getFromInterval();
-    m_a = rQuot(toInterval.getLength() , (translate(fromInterval.getTo()) - translate(fromInterval.getFrom())), m_digits);
-    m_b = rDif(toInterval.getFrom(), rProd(m_a,translate(fromInterval.getFrom()), m_digits), m_digits);
-  }
-  void checkFromInterval(const BigRealInterval &interval) {
-    if(interval.getLength().isZero()) {
-      throwException(_T("IntervalTransformation::Invalid from-interval:%s"),interval.toString().cstr());
-    }
-  }
+  void computeTransformation();
+  void checkFromInterval(const BigRealInterval &interval);
 public:
-  BigRealIntervalTransformation(const BigRealInterval &fromInterval, const BigRealInterval &toInterval, UINT digits) {
-    setPrecision(digits);
-    checkFromInterval(fromInterval);
-    m_fromInterval = fromInterval;
-    m_toInterval   = toInterval;
-  }
-  UINT setPrecision(UINT digits);
+  BigRealIntervalTransformation(const BigRealInterval &fromInterval, const BigRealInterval &toInterval, UINT precision=AUTOPRECISION);
+  // Set number of decimal digits in calculations
+  // Specify AUTOPRECISION to get 8 extra decimal digits whatever from- and toInterval are
+  UINT setPrecision(UINT precsion);
   inline UINT getPrecision() const {
+    return m_precision;
+  }
+  inline UINT getDigits() const {
     return m_digits;
   }
 
-  static BigRealInterval getDefaultInterval(IntervalScale scale) {
-    switch(scale) {
-    case LINEAR             : return BigRealInterval(-10,10);
-    default                 : throwInvalidArgumentException(__TFUNCTION__
-                                                           ,_T("scale (=%d)"), scale);
-                              return BigRealInterval(0,10);
-    }
-  }
+  static BigRealInterval getDefaultInterval(IntervalScale scale);
 
-  const BigRealInterval &getFromInterval() const {
+  inline const BigRealInterval &getFromInterval() const {
     return m_fromInterval;
   }
-  const BigRealInterval &getToInterval() const {
+  inline const BigRealInterval &getToInterval() const {
     return m_toInterval;
   }
-  virtual const BigRealInterval &setFromInterval(const BigRealInterval &interval) {
-    checkFromInterval(interval);
-    m_fromInterval = interval;
-    computeTransformation();
-    return m_fromInterval;
-  }
-  virtual const BigRealInterval &setToInterval(const BigRealInterval &interval) {
-    m_toInterval = interval;
-    computeTransformation();
-    return m_toInterval;
-  }
+  virtual const BigRealInterval &setFromInterval(const BigRealInterval &interval);
+  virtual const BigRealInterval &setToInterval(const BigRealInterval &interval);
 
-  BigReal forwardTransform(const BigReal &x) const {
-    return rSum(rProd(m_a,translate(x),m_digits), m_b, m_digits);
+  inline BigReal forwardTransform(const BigReal &x) const {
+    return rProd(m_a,translate(x),m_digits) + m_b;
   }
-  BigReal backwardTransform(const BigReal &x) const {
-    return m_a.isZero() ? getFromInterval().getFrom() : inverseTranslate(rQuot(rDif(x,m_b,m_digits),m_a,m_digits));
+  inline BigReal backwardTransform(const BigReal &x) const {
+    return m_a.isZero() ? getFromInterval().getFrom() : inverseTranslate(rQuot(x-m_b,m_a,m_digits));
   }
-  BigRealInterval forwardTransform(const BigRealInterval &interval) const {
+  inline BigRealInterval forwardTransform(const BigRealInterval &interval) const {
     return BigRealInterval(forwardTransform(interval.getFrom()),forwardTransform(interval.getTo()));
   }
-  BigRealInterval backwardTransform(const BigRealInterval &interval) const {
+  inline BigRealInterval backwardTransform(const BigRealInterval &interval) const {
     return BigRealInterval(backwardTransform(interval.getFrom()),backwardTransform(interval.getTo()));
   }
 
   virtual bool isLinear() const = 0;
   // Returns new fromInterval.
-  const BigRealInterval &zoom(const BigReal &x, const BigReal &factor, bool xInToInterval=true) {
-    if(xInToInterval) {
-      if(!getToInterval().contains(x)) {
-        return getFromInterval();
-      }
-    } else {
-      if(!getFromInterval().contains(x)) {
-        return getFromInterval();
-      }
-    }
-    BigReal x1     = xInToInterval ? translate(backwardTransform(x)) : x;
-    BigReal tFrom  = translate(getFromInterval().getFrom());
-    BigReal tTo    = translate(getFromInterval().getTo());
-    tFrom += rProd(rDif(x1, tFrom, m_digits), factor, m_digits);
-    tTo   += rProd(rDif(x1, tTo  , m_digits), factor, m_digits);
-    return setFromInterval(BigRealInterval(inverseTranslate(tFrom), inverseTranslate(tTo)));
-  }
-
+  const BigRealInterval &zoom(const BigReal &x, const BigReal &factor, bool xInToInterval=true);
   virtual IntervalScale getScale() const = 0;
   virtual BigRealIntervalTransformation *clone() const = 0;
 };
@@ -113,8 +70,8 @@ public:
   bool isLinear() const {
     return true;
   }
-  BigRealLinearTransformation(const BigRealInterval &fromInterval, const BigRealInterval &toInterval, UINT digits)
-    : BigRealIntervalTransformation(fromInterval, toInterval, digits) {
+  BigRealLinearTransformation(const BigRealInterval &fromInterval, const BigRealInterval &toInterval, UINT precision=AUTOPRECISION)
+    : BigRealIntervalTransformation(fromInterval, toInterval, precision) {
     computeTransformation();
   }
   IntervalScale getScale() const {
@@ -132,183 +89,89 @@ class BigRealRectangleTransformation {
 private:
   DECLARECLASSNAME;
   BigRealIntervalTransformation *m_xtransform, *m_ytransform;
-  UINT                           m_digits;
 
-  void checkPrecision(UINT digits);
-  void cleanup() {
-    SAFEDELETE(m_xtransform);
-    SAFEDELETE(m_ytransform);
-  }
-  void computeTransformation(const BigRealRectangle2D &from, const BigRealRectangle2D &to, IntervalScale xScale, IntervalScale yScale) {
-    BigRealIntervalTransformation *newXtransform = NULL, *newYtransform = NULL;
-    try {
-      newXtransform = allocateTransformation(BigRealInterval(from.getBottomLeft().x,from.getBottomRight().x)
-                                            ,BigRealInterval(to.getBottomLeft().x  ,to.getBottomRight().x)
-                                            ,xScale);
+  BigRealIntervalTransformation *allocateTransformation(const BigRealInterval &from, const BigRealInterval &to, IntervalScale scale) const;
+  void cleanup();
+  void computeTransformation(const BigRealRectangle2D &from, const BigRealRectangle2D &to, IntervalScale xScale, IntervalScale yScale);
 
-      newYtransform = allocateTransformation(BigRealInterval(from.getTopLeft().y, from.getBottomLeft().y)
-                                            ,BigRealInterval(to.getTopLeft().y  , to.getBottomLeft().y  )
-                                            ,yScale);
-      cleanup();
-      m_xtransform = newXtransform;
-      m_ytransform = newYtransform;
-    } catch(Exception e) {
-      SAFEDELETE(newXtransform);
-      SAFEDELETE(newYtransform);
-      throw;
-    }
-  }
+  BigRealRectangleTransformation(const BigRealIntervalTransformation &tx, const BigRealIntervalTransformation &ty);
 
-  BigRealRectangleTransformation(const BigRealIntervalTransformation &tx, const BigRealIntervalTransformation &ty) {
-    setPrecision(dmax(tx.getPrecision(),ty.getPrecision()));
-    m_xtransform = tx.clone();
-    m_ytransform = ty.clone();
-  }
+  static BigRealRectangle2D getDefaultFromRectangle(IntervalScale xScale, IntervalScale yScale);
 
-  static BigRealRectangle2D getDefaultFromRectangle(IntervalScale xScale, IntervalScale yScale) {
-    BigRealInterval xInterval = BigRealIntervalTransformation::getDefaultInterval(xScale);
-    BigRealInterval yInterval = BigRealIntervalTransformation::getDefaultInterval(yScale);
-    return BigRealRectangle2D(xInterval.getFrom(), yInterval.getFrom(), xInterval.getLength(), yInterval.getLength());
-  }
-  static BigRealRectangle2D getDefaultToRectangle() {
+  static inline BigRealRectangle2D getDefaultToRectangle() {
     return BigRealRectangle2D(0, 100, 100, -100);
-  }
-protected:
-  BigRealIntervalTransformation *allocateTransformation(const BigRealInterval &from, const BigRealInterval &to, IntervalScale scale) const {
-    switch(scale) {
-    case LINEAR             : return new BigRealLinearTransformation(from, to, m_digits);
-    default                 : throwInvalidArgumentException(__TFUNCTION__,_T("scale=%d"), scale);
-                              return NULL;
-    }
   }
 
 public:
-  BigRealRectangleTransformation(UINT digits, IntervalScale xScale = LINEAR, IntervalScale yScale = LINEAR) {
-    setPrecision(digits);
+  inline BigRealRectangleTransformation(IntervalScale xScale = LINEAR, IntervalScale yScale = LINEAR) {
     m_xtransform = m_ytransform = NULL;
     computeTransformation(getDefaultFromRectangle(xScale,yScale), getDefaultToRectangle(), xScale, yScale);
   }
-  BigRealRectangleTransformation(const BigRealRectangleTransformation &src) {
-    setPrecision(src.getPrecision());
+  inline BigRealRectangleTransformation(const BigRealRectangleTransformation &src) {
     m_xtransform = src.getXTransformation().clone();
     m_ytransform = src.getYTransformation().clone();
   }
-  BigRealRectangleTransformation &operator=(const BigRealRectangleTransformation &src) {
+  inline BigRealRectangleTransformation &operator=(const BigRealRectangleTransformation &src) {
     cleanup();
-    setPrecision(src.getPrecision());
     m_xtransform = src.getXTransformation().clone();
     m_ytransform = src.getYTransformation().clone();
     return *this;
   }
-  BigRealRectangleTransformation(UINT digits, const BigRealRectangle2D &from, const BigRealRectangle2D &to, IntervalScale xScale = LINEAR, IntervalScale yScale = LINEAR) {
-    setPrecision(digits);
+  inline BigRealRectangleTransformation(const BigRealRectangle2D &from, const BigRealRectangle2D &to, IntervalScale xScale = LINEAR, IntervalScale yScale = LINEAR) {
     m_xtransform = m_ytransform = NULL;
     computeTransformation(from, to, xScale, yScale);
-  }
-  UINT setPrecision(UINT digits);
-  inline UINT getPrecision() const {
-    return m_digits;
   }
   virtual ~BigRealRectangleTransformation() {
     cleanup();
   }
-  const BigRealIntervalTransformation &getXTransformation() const {
+  inline const BigRealIntervalTransformation &getXTransformation() const {
     return *m_xtransform;
   }
-  const BigRealIntervalTransformation &getYTransformation() const {
+  inline const BigRealIntervalTransformation &getYTransformation() const {
     return *m_ytransform;
   }
 
-  void   setFromRectangle(const BigRealRectangle2D &rect) {
+  inline void setFromRectangle(const BigRealRectangle2D &rect) {
     computeTransformation(rect,getToRectangle(),getXTransformation().getScale(),getYTransformation().getScale());
   }
-  void   setToRectangle(const BigRealRectangle2D &rect) {
+  inline void setToRectangle(const BigRealRectangle2D &rect) {
     computeTransformation(getFromRectangle(),rect,getXTransformation().getScale(),getYTransformation().getScale());
   }
-  BigRealRectangle2D  getFromRectangle() const {
-    BigRealPoint2D p1 = BigRealPoint2D(getXTransformation().getFromInterval().getFrom(), getYTransformation().getFromInterval().getFrom());
-    BigRealPoint2D p2 = BigRealPoint2D(getXTransformation().getFromInterval().getTo()  , getYTransformation().getFromInterval().getTo());
-    return BigRealRectangle2D(p1.x, p1.y, p2.x-p1.x, p2.y-p1.y);
-  }
-  BigRealRectangle2D  getToRectangle()   const {
-    BigRealPoint2D p1 = BigRealPoint2D(getXTransformation().getToInterval().getFrom()  , getYTransformation().getToInterval().getFrom());
-    BigRealPoint2D p2 = BigRealPoint2D(getXTransformation().getToInterval().getTo()    , getYTransformation().getToInterval().getTo());
-    return BigRealRectangle2D(p1.x, p1.y, p2.x-p1.x, p2.y-p1.y);
-  }
-  BigRealPoint2D      forwardTransform(const BigRealPoint2D &p)   const {
+  BigRealRectangle2D  getFromRectangle() const;
+  BigRealRectangle2D  getToRectangle()   const;
+
+  inline BigRealPoint2D      forwardTransform(const BigRealPoint2D &p)   const {
     return BigRealPoint2D(getXTransformation().forwardTransform(p.x),getYTransformation().forwardTransform(p.y));
   }
-  BigRealPoint2D      backwardTransform(const BigRealPoint2D &p)   const {
+  inline BigRealPoint2D      backwardTransform(const BigRealPoint2D &p)   const {
     return BigRealPoint2D(getXTransformation().backwardTransform(p.x),getYTransformation().backwardTransform(p.y));
   }
-  BigRealPoint2D      forwardTransform(const BigReal &x, const BigReal &y) const {
+  inline BigRealPoint2D      forwardTransform(const BigReal &x, const BigReal &y) const {
     return BigRealPoint2D(getXTransformation().forwardTransform(x),getYTransformation().forwardTransform(y));
   }
-  BigRealPoint2D      backwardTransform(const BigReal &x, const BigReal &y) const {
+  inline BigRealPoint2D      backwardTransform(const BigReal &x, const BigReal &y) const {
     return BigRealPoint2D(getXTransformation().backwardTransform(x),getYTransformation().backwardTransform(y));
   }
-  BigRealRectangle2D  forwardTransform(const BigRealRectangle2D &rect)  const {
-    BigRealPoint2D p1 = forwardTransform(rect.getTopLeft());
-    BigRealPoint2D p2 = forwardTransform(rect.getBottomRight());
+  inline BigRealRectangle2D  forwardTransform(const BigRealRectangle2D &rect)  const {
+    const BigRealPoint2D p1 = forwardTransform(rect.getTopLeft());
+    const BigRealPoint2D p2 = forwardTransform(rect.getBottomRight());
     return BigRealRectangle2D(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
   }
-  BigRealRectangle2D  backwardTransform( const BigRealRectangle2D &rect)  const {
-    BigRealPoint2D p1 = backwardTransform(rect.getTopLeft());
-    BigRealPoint2D p2 = backwardTransform(rect.getBottomRight());
+  inline BigRealRectangle2D  backwardTransform( const BigRealRectangle2D &rect)  const {
+    const BigRealPoint2D p1 = backwardTransform(rect.getTopLeft());
+    const BigRealPoint2D p2 = backwardTransform(rect.getBottomRight());
     return BigRealRectangle2D(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
   }
 
-  void setScale(IntervalScale newScale, int flags) {
-    IntervalScale xScale = getXTransformation().getScale();
-    IntervalScale yScale = getYTransformation().getScale();
-    if((flags & X_AXIS) != 0) {
-      xScale = newScale;
-    }
-    if((flags & Y_AXIS) != 0) {
-      yScale = newScale;
-    }
-    computeTransformation(getFromRectangle(),getToRectangle(),xScale,yScale);
-  }
+  void setScale(IntervalScale newScale, int flags);
   // Returns new fromRectangle.
-  BigRealRectangle2D zoom(const BigRealPoint2D &p, const BigReal &factor, int flags = X_AXIS | Y_AXIS, bool pInToRectangle=true) {
-    if(flags & X_AXIS) {
-      m_xtransform->zoom(p.x, factor, pInToRectangle);
-    }
-    if(flags & Y_AXIS) {
-      m_ytransform->zoom(p.y, factor, pInToRectangle);
-    }
-    return getFromRectangle();
-  }
-  // returns true if transformation is changed
-  virtual bool adjustAspectRatio() {
-    if(!getXTransformation().isLinear() || !getYTransformation().isLinear()) {
-      return false;
-    }
-    BigRealRectangle2D fr        = getFromRectangle();
-    BigRealRectangle2D tr        = getToRectangle();
-    const BigReal      fromRatio = fabs(rQuot(fr.getWidth() , fr.getHeight(), m_digits));
-    const BigReal      toRatio   = fabs(rQuot(tr.getWidth() , tr.getHeight(), m_digits));
-    bool               changed   = false;
-    if(fromRatio > toRatio) {
-      const BigReal dh = dsign(fr.getHeight())*rDif(fabs(rQuot(fr.getWidth(),toRatio,m_digits)), fabs(fr.getHeight()),m_digits);
-      fr.m_y -= dh * dh.getDigitPool()->getHalf();
-      fr.m_h += dh;
-      changed = !dh.isZero();
-    } else if(fromRatio < toRatio) {
-      const BigReal dw = dsign(fr.getWidth())*rDif(fabs(rProd(toRatio,fr.getHeight(),m_digits)),fabs(fr.getWidth()), m_digits);
-      fr.m_x -= dw * dw.getDigitPool()->getHalf();
-      fr.m_w += dw;
-      changed = !dw.isZero();
-    }
-    if(changed) {
-      setFromRectangle(fr);
-    }
-    return changed;
-  }
+  BigRealRectangle2D zoom(const BigRealPoint2D &p, const BigReal &factor, int flags = X_AXIS | Y_AXIS, bool pInToRectangle=true);
 
-  static BigRealRectangleTransformation getId() {
-    return BigRealRectangleTransformation(15,BigRealRectangle2D(0,0,1,1)
-                                            ,BigRealRectangle2D(0,0,1,1));
+  // returns true if transformation is changed
+  bool adjustAspectRatio();
+
+  static inline BigRealRectangleTransformation getId() {
+    return BigRealRectangleTransformation(BigRealRectangle2D(0,0,1,1)
+                                         ,BigRealRectangle2D(0,0,1,1));
   }
 };
-
--- Lib/Src/BigReal/BigRealTransformation.cpp
+++ Lib/Src/BigReal/BigRealTransformation.cpp
@@ -1,33 +1,192 @@
 #include "pch.h"
 #include <Math/BigRealTransformation.h>
 
-DEFINECLASSNAME(BigRealIntervalTransformation);
+UINT BigRealIntervalTransformation::setPrecision(UINT precision) {
+  const UINT oldPrecision = m_precision;
+  m_precision = m_digits = precision;
+  return oldPrecision;
+}
 
-void BigRealIntervalTransformation::checkPrecision(UINT digits) {
-  if(digits == 0) {
-    throwBigRealException(_T("%s:Precision = 0 not allowed"), s_className);
+void BigRealIntervalTransformation::computeTransformation() {
+  const BigRealInterval &toInterval   = getToInterval();
+  const BigRealInterval &fromInterval = getFromInterval();
+  const BigReal transFromTo           = translate(fromInterval.getTo()), transFromFrom = translate(fromInterval.getFrom());
+  const BigReal transFromLength       = transFromTo - transFromFrom;
+  const BigReal toLength              = toInterval.getLength();
+  if(m_precision == AUTOPRECISION) {
+    const BRExpoType fromLengthE10 = BigReal::getExpo10(transFromLength);
+    const BRExpoType toLengthE10   = BigReal::getExpo10(toLength);
+    const BRExpoType aE10          = toLengthE10 - fromLengthE10;
+    const BRExpoType fromFromE10   = BigReal::getExpo10(transFromFrom);
+    const BRExpoType toFromE10     = BigReal::getExpo10(toInterval.getFrom());
+    // aE10 + fromFromE10 - m_digits = toFromE10 - 8
+    m_digits = aE10 + fromFromE10 - toFromE10 + 8;
+  }
+  m_a = rQuot(toLength, transFromLength, m_digits);
+  m_b = toInterval.getFrom() - rProd(m_a,transFromFrom, m_digits);
+}
+
+void BigRealIntervalTransformation::checkFromInterval(const BigRealInterval &interval) {
+  if(interval.getLength().isZero()) {
+    throwException(_T("IntervalTransformation::Invalid from-interval:%s"),interval.toString().cstr());
   }
 }
 
-UINT BigRealIntervalTransformation::setPrecision(UINT digits) {
-  checkPrecision(digits);
-  const UINT oldDigits = m_digits;
-  m_digits = digits;
-  return oldDigits;
+BigRealIntervalTransformation::BigRealIntervalTransformation(const BigRealInterval &fromInterval, const BigRealInterval &toInterval, UINT precision) {
+  setPrecision(precision);
+  checkFromInterval(fromInterval);
+  m_fromInterval = fromInterval;
+  m_toInterval   = toInterval;
+}
+
+BigRealInterval BigRealIntervalTransformation::getDefaultInterval(IntervalScale scale) { // static
+  switch(scale) {
+  case LINEAR             : return BigRealInterval(-10,10);
+  default                 : throwInvalidArgumentException(__TFUNCTION__
+                                                          ,_T("scale (=%d)"), scale);
+                            return BigRealInterval(0,10);
+  }
+}
+
+const BigRealInterval &BigRealIntervalTransformation::setFromInterval(const BigRealInterval &interval) {
+  checkFromInterval(interval);
+  m_fromInterval = interval;
+  computeTransformation();
+  return m_fromInterval;
+}
+
+const BigRealInterval &BigRealIntervalTransformation::setToInterval(const BigRealInterval &interval) {
+  m_toInterval = interval;
+  computeTransformation();
+  return m_toInterval;
+}
+
+// Returns new fromInterval.
+const BigRealInterval &BigRealIntervalTransformation::zoom(const BigReal &x, const BigReal &factor, bool xInToInterval) {
+  if(xInToInterval) {
+    if(!getToInterval().contains(x)) {
+      return getFromInterval();
+    }
+  } else {
+    if(!getFromInterval().contains(x)) {
+      return getFromInterval();
+    }
+  }
+  BigReal x1     = xInToInterval ? translate(backwardTransform(x)) : x;
+  BigReal tFrom  = translate(getFromInterval().getFrom());
+  BigReal tTo    = translate(getFromInterval().getTo());
+  tFrom += rProd(x1 - tFrom, factor, m_digits);
+  tTo   += rProd(x1 - tTo  , factor, m_digits);
+  return setFromInterval(BigRealInterval(inverseTranslate(tFrom), inverseTranslate(tTo)));
 }
 
 DEFINECLASSNAME(BigRealRectangleTransformation);
 
-void BigRealRectangleTransformation::checkPrecision(UINT digits) {
-  if(digits == 0) {
-    throwBigRealException(_T("%s:Precision = 0 not allowed"), s_className);
+BigRealIntervalTransformation *BigRealRectangleTransformation::allocateTransformation(const BigRealInterval &from, const BigRealInterval &to, IntervalScale scale) const {
+  switch(scale) {
+  case LINEAR             : return new BigRealLinearTransformation(from, to);
+  default                 : throwInvalidArgumentException(__TFUNCTION__,_T("scale=%d"), scale);
+                            return NULL;
   }
 }
 
-UINT BigRealRectangleTransformation::setPrecision(UINT digits) {
-  checkPrecision(digits);
-  const UINT oldDigits = m_digits;
-  m_digits = digits;
-  return oldDigits;
+void BigRealRectangleTransformation::cleanup() {
+  SAFEDELETE(m_xtransform);
+  SAFEDELETE(m_ytransform);
 }
 
+void BigRealRectangleTransformation::computeTransformation(const BigRealRectangle2D &from, const BigRealRectangle2D &to, IntervalScale xScale, IntervalScale yScale) {
+  BigRealIntervalTransformation *newXtransform = NULL, *newYtransform = NULL;
+  try {
+    newXtransform = allocateTransformation(BigRealInterval(from.getBottomLeft().x,from.getBottomRight().x)
+                                          ,BigRealInterval(to.getBottomLeft().x  ,to.getBottomRight().x)
+                                          ,xScale);
+
+    newYtransform = allocateTransformation(BigRealInterval(from.getTopLeft().y, from.getBottomLeft().y)
+                                          ,BigRealInterval(to.getTopLeft().y  , to.getBottomLeft().y  )
+                                          ,yScale);
+    cleanup();
+    m_xtransform = newXtransform;
+    m_ytransform = newYtransform;
+  } catch(Exception e) {
+    SAFEDELETE(newXtransform);
+    SAFEDELETE(newYtransform);
+    throw;
+  }
+}
+
+BigRealRectangleTransformation::BigRealRectangleTransformation(const BigRealIntervalTransformation &tx, const BigRealIntervalTransformation &ty) {
+  m_xtransform = tx.clone();
+  m_ytransform = ty.clone();
+  m_xtransform->setPrecision(AUTOPRECISION);
+  m_ytransform->setPrecision(AUTOPRECISION);
+}
+
+BigRealRectangle2D BigRealRectangleTransformation::getDefaultFromRectangle(IntervalScale xScale, IntervalScale yScale) { // static
+  BigRealInterval xInterval = BigRealIntervalTransformation::getDefaultInterval(xScale);
+  BigRealInterval yInterval = BigRealIntervalTransformation::getDefaultInterval(yScale);
+  return BigRealRectangle2D(xInterval.getFrom(), yInterval.getFrom(), xInterval.getLength(), yInterval.getLength());
+}
+
+BigRealRectangle2D  BigRealRectangleTransformation::getFromRectangle() const {
+  BigRealPoint2D p1 = BigRealPoint2D(getXTransformation().getFromInterval().getFrom(), getYTransformation().getFromInterval().getFrom());
+  BigRealPoint2D p2 = BigRealPoint2D(getXTransformation().getFromInterval().getTo()  , getYTransformation().getFromInterval().getTo());
+  return BigRealRectangle2D(p1.x, p1.y, p2.x-p1.x, p2.y-p1.y);
+}
+
+BigRealRectangle2D  BigRealRectangleTransformation::getToRectangle()   const {
+  BigRealPoint2D p1 = BigRealPoint2D(getXTransformation().getToInterval().getFrom()  , getYTransformation().getToInterval().getFrom());
+  BigRealPoint2D p2 = BigRealPoint2D(getXTransformation().getToInterval().getTo()    , getYTransformation().getToInterval().getTo());
+  return BigRealRectangle2D(p1.x, p1.y, p2.x-p1.x, p2.y-p1.y);
+}
+
+void BigRealRectangleTransformation::setScale(IntervalScale newScale, int flags) {
+  IntervalScale xScale = getXTransformation().getScale();
+  IntervalScale yScale = getYTransformation().getScale();
+  if((flags & X_AXIS) != 0) {
+    xScale = newScale;
+  }
+  if((flags & Y_AXIS) != 0) {
+    yScale = newScale;
+  }
+  computeTransformation(getFromRectangle(),getToRectangle(),xScale,yScale);
+}
+// Returns new fromRectangle.
+BigRealRectangle2D BigRealRectangleTransformation::zoom(const BigRealPoint2D &p, const BigReal &factor, int flags, bool pInToRectangle) {
+  if(flags & X_AXIS) {
+    m_xtransform->zoom(p.x, factor, pInToRectangle);
+  }
+  if(flags & Y_AXIS) {
+    m_ytransform->zoom(p.y, factor, pInToRectangle);
+  }
+  return getFromRectangle();
+}
+
+// returns true if transformation is changed
+bool BigRealRectangleTransformation::adjustAspectRatio() {
+  if(!getXTransformation().isLinear() || !getYTransformation().isLinear()) {
+    return false;
+  }
+
+  BigRealRectangle2D fr        = getFromRectangle();
+  BigRealRectangle2D tr        = getToRectangle();
+  const BigReal      fromRatio = fabs(rQuot(fr.getWidth() , fr.getHeight(), 15));
+  const BigReal      toRatio   = fabs(rQuot(tr.getWidth() , tr.getHeight(), 15));
+  bool               changed   = false;
+  const UINT         digits    = max(getXTransformation().getDigits(), getYTransformation().getDigits());
+  if(fromRatio > toRatio) {
+    const BigReal dh = dsign(fr.getHeight())*(fabs(rQuot(fr.getWidth(),toRatio,digits)) - fabs(fr.getHeight()));
+    fr.m_y -= dh * dh.getDigitPool()->getHalf();
+    fr.m_h += dh;
+    changed = !dh.isZero();
+  } else if(fromRatio < toRatio) {
+    const BigReal dw = dsign(fr.getWidth())*(fabs(rProd(toRatio,fr.getHeight(),digits)),fabs(fr.getWidth()));
+    fr.m_x -= dw * dw.getDigitPool()->getHalf();
+    fr.m_w += dw;
+    changed = !dw.isZero();
+  }
+  if(changed) {
+    setFromRectangle(fr);
+  }
+  return changed;
+}
