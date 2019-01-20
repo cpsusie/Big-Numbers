3613881470e674a467db814bd526c1c5466f9841
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-01-20 21:33:28 +0100

    Added new option for Tokenizer, parameter flags:TOK_CSTRING Added static function TCHAR *Tokenizer::parseCstring(const TCHAR *cp, String *result=NULL) to parse c-like strings, and if result specified, it will contain the parsed string, without leading/trailing " and translated escape sequences

--- Lib/Include/Tokenizer.h
+++ Lib/Include/Tokenizer.h
@@ -3,8 +3,11 @@
 #include "MyUtil.h"
 #include "BitSet.h"
 
-#define TOK_INVERTSET        1  // set this flag to use complement of delimiterset
-#define TOK_SINGLEDELIMITERS 2  // set this flag to allow only 1 delimiter between tokens, ie empty tokens
+#define TOK_INVERTSET        0x01  // set this flag to use complement of delimiterset
+#define TOK_SINGLEDELIMITERS 0x02  // set this flag to allow only 1 delimiter between tokens, ie empty tokens
+#define TOK_CSTRING          0x04  // set textQualifier to " and handle all c string escape sequences as \",\n,\r,\t...
+                                   // as defined in https://en.wikipedia.org/wiki/Escape_sequences_in_C
+                                   // escape sequences are not translated, and leading/ending " are not removed from string
 
 class StringIndex {
 private:
@@ -32,18 +35,19 @@ public:
 class Tokenizer {
 private:
   const TCHAR        *m_str;
+  UINT                m_flags;
   BitSet              m_delimiterSet;
   TCHAR               m_textQualifier;
   bool                m_singleDelimiter;
   const TCHAR        *m_next;
   TCHAR              *m_stringBuffer;
   size_t              m_stringBufferSize; // in characters. not bytes
-  void init(const TCHAR *str, const TCHAR *delimiters, TCHAR textQualifier, int flags);
+  void init(const TCHAR *str, const TCHAR *delimiters, TCHAR textQualifier);
   StringIndex &nextIndex(StringIndex &tIndex);
   void cleanup();
 public:
-  Tokenizer(const TCHAR  *str, const TCHAR  *delimiters, TCHAR textQualifier = 0, int flags = 0);
-  Tokenizer(const String &str, const String &delimiters, TCHAR textQualifier = 0, int flags = 0);
+  Tokenizer(const TCHAR  *str, const TCHAR  *delimiters, TCHAR textQualifier = 0, UINT flags = 0);
+  Tokenizer(const String &str, const String &delimiters, TCHAR textQualifier = 0, UINT flags = 0);
   Tokenizer(const Tokenizer &src);
   Tokenizer &operator=(const Tokenizer &src);
   virtual ~Tokenizer();
@@ -59,4 +63,9 @@ public:
   __int64          getInt64( bool hex = false);
   unsigned __int64 getUint64(bool hex = false);
   bool             getBool(); // expect "true" or "false"
+  // assume *cp == '"', beginning of C-string, scan c-string, returning pointer to TCHAR following terminating "
+  // throws exception if invalid escape sequence,newline or not terminated string
+  // escape sequences are defined in https://en.wikipedia.org/wiki/Escape_sequences_in_C
+  // if result != NULL, it will contain the string without leading/ending " and escape sequnces translated
+  static TCHAR *parseCstring(const TCHAR *cp, String *result=NULL);
 };
--- Lib/Src/Util/Tokenizer.cpp
+++ Lib/Src/Util/Tokenizer.cpp
@@ -7,15 +7,24 @@
 #define MAXCHARVALUE 0xff
 #endif
 
-Tokenizer::Tokenizer(const TCHAR *str, const TCHAR *delimiters, TCHAR textQualifier, int flags) : m_delimiterSet(MAXCHARVALUE) {
-  init(str,delimiters,textQualifier,flags);
+Tokenizer::Tokenizer(const TCHAR *str, const TCHAR *delimiters, TCHAR textQualifier, UINT flags)
+: m_delimiterSet(MAXCHARVALUE)
+, m_flags(flags)
+{
+  init(str,delimiters,textQualifier);
 }
 
-Tokenizer::Tokenizer(const String &str, const String &delimiters, TCHAR textQualifier, int flags) : m_delimiterSet(MAXCHARVALUE) {
-  init(str.cstr(), delimiters.cstr(), textQualifier, flags);
+Tokenizer::Tokenizer(const String &str, const String &delimiters, TCHAR textQualifier, UINT flags) 
+: m_delimiterSet(MAXCHARVALUE)
+, m_flags(flags)
+{
+  init(str.cstr(), delimiters.cstr(), textQualifier);
 }
 
-Tokenizer::Tokenizer(const Tokenizer &src) : m_delimiterSet(src.m_delimiterSet) {
+Tokenizer::Tokenizer(const Tokenizer &src)
+: m_delimiterSet(src.m_delimiterSet)
+, m_flags(src.m_flags)
+{
   m_str              = src.m_str;
   m_textQualifier    = src.m_textQualifier;
   m_singleDelimiter  = src.m_singleDelimiter;
@@ -29,6 +38,7 @@ Tokenizer &Tokenizer::operator=(const Tokenizer &src) {
     return *this;
   }
   m_str             = src.m_str;
+  m_flags           = src.m_flags;
   m_delimiterSet    = src.m_delimiterSet;
   m_textQualifier   = src.m_textQualifier;
   m_singleDelimiter = src.m_singleDelimiter;
@@ -45,22 +55,25 @@ void Tokenizer::cleanup() {
   m_stringBufferSize = 0;
 }
 
-void Tokenizer::init(const TCHAR *str, const TCHAR *delimiters, TCHAR textQualifier, int flags) {
+void Tokenizer::init(const TCHAR *str, const TCHAR *delimiters, TCHAR textQualifier) {
   m_str              = str;
-  m_textQualifier    = textQualifier;
+  m_textQualifier    = (m_flags & TOK_CSTRING) ? '"' : textQualifier;
   m_stringBuffer     = NULL;
   m_stringBufferSize = 0;
   for(const TCHAR *cp = (const TCHAR*)delimiters; *cp; cp++) {
     m_delimiterSet += (ULONG)(*cp);                    // m_delimiterSet never contains '\0'
   }
 
-  if(flags & TOK_INVERTSET) {
+  if(m_flags & TOK_INVERTSET) {
     m_delimiterSet.invert();
-    m_delimiterSet.remove(0);                                  // m_delimiterSet never contains '\0'
+    m_delimiterSet.remove(0);                          // m_delimiterSet never contains '\0'
   }
-  m_singleDelimiter = (flags & TOK_SINGLEDELIMITERS) ? true : false;
+  m_singleDelimiter = (m_flags & TOK_SINGLEDELIMITERS) ? true : false;
   m_next = str;
 
+  if(m_delimiterSet.contains(m_textQualifier)) {
+    throwException(_T("TextQualifier '%c' cannot be contained in delimiterset (={%s})"), m_textQualifier, delimiters);
+  }
   if(!m_singleDelimiter) {
     while(m_delimiterSet.contains((ULONG)(*m_next))) { // m_delimiterSet never contains '\0'
       m_next++;
@@ -105,7 +118,12 @@ StringIndex &Tokenizer::nextIndex(StringIndex &tIndex) {
   bool inString = false;
   while(*m_next) {
     if(*m_next == m_textQualifier) {
-      inString = !inString;
+      if(m_flags&TOK_CSTRING) {
+        m_next = parseCstring(m_next);
+        continue;
+      } else {
+        inString = !inString;
+      }
     } else {
       if(!inString && m_delimiterSet.contains(*m_next)) {
         break;
@@ -128,6 +146,109 @@ StringIndex &Tokenizer::nextIndex(StringIndex &tIndex) {
     }
   }
   return tIndex;
+}
+
+static inline bool isodigit(TCHAR ch) {
+ return ('0' <= ch) && (ch <= '7');
+}
+
+static inline UINT octalToInt(UINT ch) {
+  return (ch- '0') & 0x7;
+}
+
+static inline UINT hexToInt(UINT ch) {
+  return (iswdigit(ch) ? (ch - '0'): ((toupper(ch)) - 'A') + 10) & 0xf;
+}
+
+TCHAR *Tokenizer::parseCstring(const TCHAR *cp, String *result) { // static
+  const TCHAR *start = cp++;
+  for(;;) {
+    switch(*cp) {
+    case    0:
+    case '\n':
+      throwException(_T("Unterminated string:%s"), start);
+    case '\"':
+      cp++;
+      return (TCHAR*)cp;
+    case '\\':
+      cp++;
+      switch(*cp) {
+      case 'a':  if(result) *result += _T('\a'); cp++; continue;
+      case 'b' : if(result) *result += _T('\b'); cp++; continue;
+      case 'f' : if(result) *result += _T('\f'); cp++; continue;
+      case 'n' : if(result) *result += _T('\n'); cp++; continue;
+      case 'r' : if(result) *result += _T('\r'); cp++; continue;
+      case 't' : if(result) *result += _T('\t'); cp++; continue;
+      case 'v' : if(result) *result += _T('\v'); cp++; continue;
+      case '\\': if(result) *result += _T('\\'); cp++; continue;
+      case '\'': if(result) *result += _T('\''); cp++; continue;
+      case 'e' : if(result) *result += _T('\33'); cp++; continue;
+      case '"' : if(result) *result += _T('\"'); cp++; continue;
+      case '?' : if(result) *result += _T('\?'); cp++; continue;
+      case '0' :
+      case '1' :
+      case '2' :
+      case '3' :
+      case '4' :
+      case '5' :
+      case '6' :
+      case '7' :
+        { UINT v = octalToInt(*cp);
+          for(cp++; isodigit(*cp) && (v <= 0xff); cp++) {
+            v = (v<<3) | octalToInt(*cp);
+          }
+          if(v > 0xff) {
+            cp--; v >>= 3;
+          }
+          if(result) *result += (TCHAR)v;
+        }
+        continue;
+      case 'x' :
+        { UINT v = hexToInt(*cp);
+          for(cp++; isxdigit((UINT)*cp) && (v <= 0xff); cp++) {
+            v = (v<<4) | hexToInt(*cp);
+          }
+          if(v > 0xff) {
+            cp--;
+            v >>= 4;
+          }
+          if(result) *result += (TCHAR)v;
+        }
+        continue;
+      case 'u' :
+        { UINT v = hexToInt(*cp);
+          for(cp++; isxdigit(*cp) && (v <= 0xffff); cp++) {
+            v = (v<<4) | hexToInt(*cp);
+          }
+          if(v > 0xffff) {
+            cp--;
+            v >>= 4;
+          }
+          if(result) *result += (TCHAR)v;
+        }
+        continue;
+      case 'U' :
+        { UINT64 v = hexToInt(*cp);
+          for(cp++; isxdigit(*cp) && (v <= 0xffffffffu); cp++) {
+            v = (v<<4) | hexToInt(*cp);
+          }
+          if(v > 0xffffffffu) {
+            cp--;
+            v >>= 4;
+          }
+          // ups.....v can be 32-bit unsigned
+          if(result) *result += (TCHAR)v;
+        }
+        continue;
+      default  :
+        throwException(_T("Invalid escape sequence in C string:%s"), cp);
+      } // end case '\\'
+    default:
+      if(result) *result += *cp;
+      cp++;
+      break;
+    } // switch(*cp)
+  } // for(;;)
 }
 
 double Tokenizer::getDouble() {
