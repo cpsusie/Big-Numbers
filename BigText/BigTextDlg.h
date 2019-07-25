73c6ea3f71b218b58f79e7e886886ad32564d8d8
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-07-25 23:31:42 +0200

    Adjusted indent

--- BigText/BigTextDlg.h
+++ BigText/BigTextDlg.h
@@ -2,22 +2,22 @@
 
 class CBigTextDlg : public CDialogEx {
 private:
-	HICON m_hIcon;
+  HICON m_hIcon;
   CString m_text;
 public:
-	CBigTextDlg(CWnd* pParent = NULL);
+  CBigTextDlg(CWnd* pParent = NULL);
 
-	enum { IDD = IDD_BIGTEXT_DIALOG };
+  enum { IDD = IDD_BIGTEXT_DIALOG };
 
 protected:
-	virtual void DoDataExchange(CDataExchange* pDX);
-	virtual BOOL OnInitDialog();
-	afx_msg HCURSOR OnQueryDragIcon();
-	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
-	afx_msg void OnPaint();
+  virtual void DoDataExchange(CDataExchange* pDX);
+  virtual BOOL OnInitDialog();
+  afx_msg HCURSOR OnQueryDragIcon();
+  afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+  afx_msg void OnPaint();
   afx_msg void OnClickedMakeBigText();
   afx_msg void OnClose();
   virtual void OnCancel();
   virtual void OnOK();
-	DECLARE_MESSAGE_MAP()
+  DECLARE_MESSAGE_MAP()
 };
