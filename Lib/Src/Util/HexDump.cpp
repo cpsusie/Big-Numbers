e5e2315f308bf53dbd5ed9769634ac693d8f11ce
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-10-18 00:54:55 +0200

    Moved function hexdumpString to its own cpp-file

--- Lib/Src/Util/HexDump.cpp
+++ Lib/Src/Util/HexDump.cpp
@@ -1,33 +1,6 @@
 #include "pch.h"
 #include <DebugLog.h>
 
-#define LINESIZE 16
-
-#define LINESTRING()  (spaceString(60, _T('-'))+_T("\n"))
-
-String hexdumpString(const void *data, size_t size) {
-  String result = format(_T("hexdump(size=%zu)\n"), size);
-  const BYTE *p = (const BYTE*)data;
-  result += LINESTRING();
-  for(size_t i = 0; i < size; i += LINESIZE) {
-    result += format(_T("%p:"),p+i);
-    size_t j;
-    for(j = i; j < i+LINESIZE && j < size; j++) {
-      result += format(_T("%02x "),p[j]);
-    }
-    for(; j < i+LINESIZE; j++) {
-      result += _T("   ");
-    }
-    result += _T("      ");
-    for(j = i; j < i+LINESIZE && j < size; j++) {
-      result += format(_T("%c "), isprint(p[j])?p[j]:'.');
-    }
-    result += _T("\n");
-  }
-  result += LINESTRING();
-  return result;
-}
-
 void hexdump(const void *data, size_t size, FILE *f) {
   const String str = hexdumpString(data, size);
   if(f) {
