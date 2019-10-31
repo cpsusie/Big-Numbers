be26b78cb68965a5337db19796cbca07ffc5dc52
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-11-01 00:41:22 +0100

    Added function AddIn_BigInt to BigRealAddin + BigReal.natvis

--- Lib/Src/BigRealAddin/BigRealAddin.cpp
+++ Lib/Src/BigRealAddin/BigRealAddin.cpp
@@ -11,6 +11,9 @@ public:
   inline bool hasNext() const {
     return next != 0;
   }
+  inline bool hasPrev() const {
+    return prev != 0;
+  }
 };
 
 template<class ETYPE, class VTYPE> class BigRealType {
@@ -65,15 +68,22 @@ private:
     getDigit(d, d.next);
     return true;
   }
+  inline bool prevDigit(DigitType<VTYPE> &d) const {
+    if(!d.hasPrev()) return false;
+    getDigit(d, d.prev);
+    return true;
+  }
   inline void addstr(const char *s) {
     m_result += s;
   }
   inline void addstr(const string &s) {
     m_result += s;
   }
-  inline void addDigitStr(VTYPE n, UINT width = 0) {
+  // return digits added
+  inline int addDigitStr(VTYPE n, UINT width = 0) {
     char tmp[100];
     addstr(digitToStr(tmp, n, width));
+    return (int)strlen(tmp);
   }
   inline void addDecimalPoint() {
     if(!m_hasDecimalPoint) {
@@ -81,8 +91,48 @@ private:
       m_hasDecimalPoint = true;
     }
   }
-  inline void addZeroes(int count) {
-    m_result.insert(m_result.length(), count, '0');
+
+#define STACKEDDIGITS()  (digitStack.getHeight() * log10Base)
+  // return string containing as much as possible up to maxlen digits from the integer. n.m_low >= 0
+  string getTailString(const BRType &n, size_t maxlen) const {
+    CompactStack<VTYPE> digitStack;
+    for(int i = 0; (STACKEDDIGITS() < maxlen) && (i < n.m_low); i++) {
+      digitStack.push(0);
+    }
+    if((STACKEDDIGITS() < maxlen)) {
+      DigitType<VTYPE> digit;
+      getDigit(digit, n.m_last);
+      for(digitStack.push(digit.n); (STACKEDDIGITS() < maxlen) && prevDigit(digit);) {
+        digitStack.push(digit.n);
+      }
+    }
+    string result;
+    while(!digitStack.isEmpty()) {
+      char str[40];
+      result += digitToStr(str, digitStack.pop(), log10Base);
+    }
+    if(result.length() > maxlen) {
+      result.erase(0, result.length() - maxlen); // delete first characters...not last...we want the tail-digits
+    }
+    return result;
+  }
+
+  static string zeroString(UINT count) {
+    string result;
+    if(count > 0) result.insert(0, count, '0');
+    return result;
+  }
+
+  static string infoString(ETYPE expo10) {
+    char str[100];
+    sprintf(str, "..Total:%I64d digits..", (INT64)expo10);
+    return str;
+  }
+
+  inline void addZeroes(ETYPE count) {
+    if(count > 0) {
+      m_result.insert(m_result.length(), (size_t)count, '0');
+    }
   }
   inline void removeTrailingZeroes() {
     if(m_hasDecimalPoint) {
@@ -115,11 +165,11 @@ private:
     sprintf(tmp, "e%+03I64d", expo10);
     return tmp;
   }
-  void formatNonNormal(const BRType &n) {
+  void formatNonNormal(const BRType &n, bool showDecimals) {
     char tmp[100];
     switch(n.m_low) {
     case BIGREAL_ZEROLOW:
-      addstr("0.0000000000000000000");
+      addstr(showDecimals ? "0.0000000000000000000" : "0");
       break;
     case BIGREAL_INFLOW:
       addstr(n.m_negative ? "-inf" : "inf");
@@ -137,10 +187,10 @@ private:
 public:
   BigRealAddIn(DEBUGHELPER *pHelper, string &dst) : m_helper(pHelper), m_result(dst), m_hasDecimalPoint(false) {
   }
-  void toString(BRType &n, size_t maxResult) {
+  void toRealString(BRType &n, size_t maxResult) {
     const ETYPE expo = n.m_expo;
     if(expo == nonNormalExpo) {
-      formatNonNormal(n);
+      formatNonNormal(n, true);
       return;
     }
 
@@ -217,7 +267,7 @@ public:
         precision = (int)maxResult - (int)m_result.length();
         if(expo10 < -1) {
           const ETYPE zeroCount = -expo10 - 1;
-          addZeroes((int)zeroCount);
+          addZeroes(zeroCount);
           decimalsDone += (int)zeroCount;
         }
         addDigitStr(digit.n);
@@ -229,7 +279,7 @@ public:
           addDigitStr(digit.n, log10Base);
         }
         if(d > 0) {
-          addZeroes((int)d);
+          addZeroes(d);
         } else {
           addDecimalPoint();
         }
@@ -249,7 +299,54 @@ public:
       }
     }
   }
+
+  void toIntString(BRType &n, size_t maxResult) {
+    const ETYPE expo = n.m_expo;
+    if(expo == nonNormalExpo) {
+      formatNonNormal(n, false);
+      return;
+    }
+
+    DigitType<VTYPE> digit;
+    getDigit(digit, n.m_first);
+
+    const int    firstDigitCount = BigReal::getDecimalDigitCount(digit.n);
+    const int    firstExpo10     = firstDigitCount - 1;
+
+    int totalDecimalDigitCount = (int)((expo - n.m_low) * log10Base + firstDigitCount);
+
+    if(n.m_negative) {
+      addstr("-");
+    }
+    const ETYPE expo10      = expo * log10Base + firstExpo10;
+          ETYPE digitsToAdd = expo10;
+
+    digitsToAdd -= addDigitStr(digit.n);
+    for(;(m_result.length() < maxResult) && nextDigit(digit);) {
+      digitsToAdd -= addDigitStr(digit.n, log10Base);
+    }
+    // digitsToAdd = digits yet to add to get complete string
+    if(digitsToAdd > 0) { // still need part of the tail
+      intptr_t spaceLeft = (intptr_t)maxResult - (intptr_t)m_result.length();
+      if(digitsToAdd <= spaceLeft) { // no need for info-string
+        // It must be zeroes, because above loop stopped while spaceLeft >= digitsToAdd > 0 => maxResult > |m_result| => nextDigit returned false
+        addZeroes(digitsToAdd);
+      } else {
+        if(spaceLeft < 0) { // first (if needed) cut resultstring to have length maxResult
+          removeLast(-spaceLeft); digitsToAdd -= spaceLeft; // m_result.length() == maxResult, digitsToAdd adjusted = #digits not printed
+        }
+        const string info = infoString(expo10);
+        const string tail = getTailString(n, (int)min(digitsToAdd, 10));
+        removeLast(info.length() + tail.length());
+        addstr(info);
+        addstr(tail);
+      }
+    }
+  }
 };
+
+typedef BigRealAddIn<BigRealx86, BRExpoTypex86, BRDigitTypex86, LOG10_BIGREALBASEx86, BIGREAL_NONNORMALx86> BigRealAddInx86;
+typedef BigRealAddIn<BigRealx64, BRExpoTypex64, BRDigitTypex64, LOG10_BIGREALBASEx64, BIGREAL_NONNORMALx64> BigRealAddInx64;
 
 ADDIN_API HRESULT WINAPI AddIn_BigReal(DWORD dwAddress, DEBUGHELPER *pHelper, int nBase, BOOL bUniStrings, char *pResult, size_t maxResult, DWORD /*reserved*/) {
   try {
@@ -258,13 +355,37 @@ ADDIN_API HRESULT WINAPI AddIn_BigReal(DWORD dwAddress, DEBUGHELPER *pHelper, in
     case PRTYPE_X86:
       { BigRealx86 n;
         pHelper->getRealObject(&n, sizeof(n));
-        BigRealAddIn<BigRealx86, BRExpoTypex86, BRDigitTypex86, LOG10_BIGREALBASEx86, BIGREAL_NONNORMALx86>(pHelper, tmpStr).toString(n, maxResult - 1);
+        BigRealAddInx86(pHelper, tmpStr).toRealString(n, maxResult - 1);
       }
       break;
     case PRTYPE_X64:
       { BigRealx64 n;
         pHelper->getRealObject(&n, sizeof(n));
-        BigRealAddIn<BigRealx64, BRExpoTypex64, BRDigitTypex64, LOG10_BIGREALBASEx64, BIGREAL_NONNORMALx64>(pHelper, tmpStr).toString(n, maxResult - 1);
+        BigRealAddInx64(pHelper, tmpStr).toRealString(n, maxResult - 1);
+      }
+      break;
+    }
+    strncpy(pResult, tmpStr.c_str(), maxResult);
+  } catch (...) {
+    strncpy(pResult, "", maxResult);
+  }
+  return S_OK;
+}
+
+ADDIN_API HRESULT WINAPI AddIn_BigInt(DWORD dwAddress, DEBUGHELPER *pHelper, int nBase, BOOL bUniStrings, char *pResult, size_t maxResult, DWORD /*reserved*/) {
+  try {
+    string tmpStr;
+    switch(pHelper->getProcessorType()) {
+    case PRTYPE_X86:
+      { BigRealx86 n;
+        pHelper->getRealObject(&n, sizeof(n));
+        BigRealAddInx86(pHelper, tmpStr).toIntString(n, maxResult - 1);
+       }
+      break;
+    case PRTYPE_X64:
+      { BigRealx64 n;
+        pHelper->getRealObject(&n, sizeof(n));
+        BigRealAddInx64(pHelper, tmpStr).toIntString(n, maxResult - 1);
       }
       break;
     }
--- Lib/Src/BigRealAddin/BigRealAddin.h
+++ Lib/Src/BigRealAddin/BigRealAddin.h
@@ -3,4 +3,5 @@
 #include <NatvisAddInHelper.h>
 
 /* Exported Functions */
-ADDIN_API HRESULT WINAPI AddIn_BigReal( DWORD dwAddress, DEBUGHELPER *pHelper, int nBase, BOOL bUniStrings, char *pResult, size_t maxResult, DWORD /*reserved*/);
+ADDIN_API HRESULT WINAPI AddIn_BigReal(DWORD dwAddress, DEBUGHELPER *pHelper, int nBase, BOOL bUniStrings, char *pResult, size_t maxResult, DWORD /*reserved*/);
+ADDIN_API HRESULT WINAPI AddIn_BigInt( DWORD dwAddress, DEBUGHELPER *pHelper, int nBase, BOOL bUniStrings, char *pResult, size_t maxResult, DWORD /*reserved*/);
