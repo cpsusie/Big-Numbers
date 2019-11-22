ce0e0ab6a6bf736defa1c66311ed4410506b7b4f
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-11-22 21:47:10 +0100

    Fixed minor syntax-errors

--- Math/MultiplicationServer/MultiplicationServer.cpp
+++ Math/MultiplicationServer/MultiplicationServer.cpp
@@ -2,13 +2,15 @@
 #include <MyUtil.h>
 #include <Math/BigReal.h>
 
+using namespace std;
+
 int main(int argc, char **argv) {
   for(;;) {
     if(tcin.eof()) break;
     BigReal x, y, f;
     tcin >> x >> y >> f;
 
-    FullFormatBigReal z = prod(x, y, f);
+    const FullFormatBigReal z = prod(x, y, f);
     tcout << z << endl;
   }
   return 0;
