263890b5ff8f5a3fc37b38fb18a3da5bf315cf96
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-12-03 21:08:59 +0100

    Added class ConstBigRealInterval to BigRealInterval.h function getNeededDecimalDigits implemented with function template Added parameter digitPool =NULL to function getLength

--- Lib/Include/Math/BigRealInterval.h
+++ Lib/Include/Math/BigRealInterval.h
@@ -3,6 +3,14 @@
 #include <NumberInterval.h>
 #include "BigReal.h"
 
+// Find needed decimal digitCount to use, so that add/sub of from and to will be done with at
+// least precision significant decimal digits.
+template<class T> size_t neededDecimalDigitsTemplate(const NumberInterval<T> *i, size_t digits) {
+  const BigReal  len = i->getLength(); // don't care about sign. need only expo10
+  const BigReal &m   = (BigReal::compareAbs(i->getFrom(), i->getTo()) > 0) ? i->getFrom() : i->getTo();
+  return BigReal::getExpo10(m) - BigReal::getExpo10(len) + digits;
+}
+
 class BigRealInterval : public NumberInterval<BigReal> {
 public:
   BigRealInterval(DigitPool *digitPool=NULL)
@@ -23,17 +31,46 @@ public:
     return getFrom().getDigitPool();
   }
 
+  inline BigReal getLength(DigitPool *digitPool = NULL) const {
+    _SELECTDIGITPOOL(getFrom());
+    return dif(getTo(),getFrom(),pool->_0(), pool);
+  }
+
   template<class T> BigRealInterval &operator=(const T &src) {
     DigitPool *dp = getDigitPool();
     setFrom(BigReal(src.getFrom(),dp));
     setTo(  BigReal(src.getTo()  ,dp));
     return *this;
   }
+  inline size_t getNeededDecimalDigits(size_t digits) const {
+    return neededDecimalDigitsTemplate(this, digits);
+  }
+};
+
+class ConstBigRealInterval : public NumberInterval<ConstBigReal> {
+private:
+  ConstBigRealInterval &setFrom(const ConstBigReal &v);    // not defined
+  ConstBigRealInterval &setTo(  const ConstBigReal &v);    // not defined
+  ConstBigRealInterval &operator+=(const NumberInterval<ConstBigReal> &rhs); // union operator..not defined
+  ConstBigRealInterval &operator=(ConstBigRealInterval &); // not defined
+public:
+  ConstBigRealInterval(const BigReal &from, const BigReal &to) : NumberInterval(ConstBigReal(from), ConstBigReal(to)) {
+  }
+  ConstBigRealInterval(const ConstBigRealInterval &src) : NumberInterval(ConstBigReal(src.getFrom()), ConstBigReal(src.getTo())) {
+  }
+  ConstBigRealInterval(const BigRealInterval &src) : NumberInterval(ConstBigReal(src.getFrom()), ConstBigReal(src.getTo())) {
+  }
+  inline DigitPool *getDigitPool() const {
+    return getFrom().getDigitPool();
+  }
+
+  inline BigReal getLength(DigitPool *digitPool = NULL) const {
+    _SELECTDIGITPOOL(getFrom());
+    return dif(getTo(), getFrom(), pool->_0(), pool);
+  }
   // Find needed decimal digitCount to use, so that add/sub of from and to will be done with at
   // least precision significant decimal digits.
   inline size_t getNeededDecimalDigits(size_t digits) const {
-    const BigReal length = getLength(); // don't care about sign. need only expo10
-    const BigReal &m     = (BigReal::compareAbs(getFrom(),getTo()) > 0) ? getFrom() : getTo();
-    return BigReal::getExpo10(m) - BigReal::getExpo10(length) + digits;
+    return neededDecimalDigitsTemplate(this, digits);
   }
 };
