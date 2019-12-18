975b7f0601a2dcaaabd899dc602bb57095cd9b93
Jesper <jesper.gr.mikkelsen@gmail.com>
2019-12-18 22:29:22 +0100

    Modified type of m_gate to be FastSemaphore and renamed it to m_lock

--- Lib/Include/SynchronizedQueue.h
+++ Lib/Include/SynchronizedQueue.h
@@ -1,56 +1,57 @@
 #pragma once
 
 #include "Semaphore.h"
+#include "FastSemaphore.h"
 #include "QueueList.h"
 
 template <typename T> class SynchronizedQueue : private QueueList<T> {
 private:
-  mutable Semaphore m_gate;
-  Semaphore         m_emptySem;
+  mutable FastSemaphore m_lock;
+  Semaphore             m_emptySem;
 public:
   SynchronizedQueue() : m_emptySem(0) {
   }
 
   void put(const T &v) {
-    m_gate.wait();
+    m_lock.wait();
     __super::put(v);
     m_emptySem.notify(); // tell any thread, waiting in get that we are not empty anymore
-    m_gate.notify(); // open gate
+    m_lock.notify(); // open gate
   }
 
   T get(int milliseconds=INFINITE) {
     for(;;) {
-      m_gate.wait();
+      m_lock.wait();
       if(!isEmpty()) {
         break;
       }
-      m_gate.notify(); // open gate
+      m_lock.notify(); // open gate
       if(!m_emptySem.wait(milliseconds)) { //wait=false => timeout
         throwTimeoutException(_T("timeout"));
       }
     }
     T result = __super::get();
-    m_gate.notify(); // open gate
+    m_lock.notify(); // open gate
     return result;
   }
 
   void clear() {
-    m_gate.wait();
+    m_lock.wait();
     __super::clear();
-    m_gate.notify();
+    m_lock.notify();
   }
 
   bool addAll(const Collection<T> &c) {
-    m_gate.wait();
+    m_lock.wait();
     const bool result = __super::addAll(c);
-    m_gate.notify();
+    m_lock.notify();
     return result;
   }
 
   bool addAll(const CompactArray<T> &a) {
-    m_gate.wait();
+    m_lock.wait();
     const bool result = __super::addAll(a);
-    m_gate.notify();
+    m_lock.notify();
     return result;
   }
 
@@ -63,9 +64,9 @@ public:
   }
 
   T operator[](size_t index) const {
-    m_gate.wait();
+    m_lock.wait();
     T result = __super::operator[](index);
-    m_gate.notify();
+    m_lock.notify();
     return result;
   }
 };
