9675b6e506ee5dc3afefe84e2f69f1f5d2634f26
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-03-11 14:40:21 +0100

    update include <D3Error.h> (now in MFCUtil)

--- D3Function/stdafx.h
+++ D3Function/stdafx.h
@@ -4,8 +4,8 @@
 
 #include <MFCUtil/WinTools.h>
 #include <MFCUtil/D3DeviceFactory.h>
+#include <MFCUtil/D3Error.h>
 #include <D3DGraphics/D3Math.h>
-#include <D3DGraphics/D3Error.h>
 #include <D3DGraphics/D3Scene.h>
 #include <D3DGraphics/D3SceneObjectWithMesh.h>
 #include <D3DGraphics/D3SceneObjectAnimatedMesh.h>
