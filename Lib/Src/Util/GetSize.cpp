19894a88ebd6dc6a930a265c0dfdf542dfe0303b
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-03-12 06:52:42 +0100

    Changed returntype for function GETSIZE from __inte64 -> INT64 Changed type of argument offset from __int64 -> INT64

--- Lib/Src/Util/GetSize.cpp
+++ Lib/Src/Util/GetSize.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include <io.h>
 
-__int64 GETSIZE(FILE *f) {
-  __int64 result = _filelengthi64(_fileno(f));
+INT64 GETSIZE(FILE *f) {
+  INT64 result = _filelengthi64(_fileno(f));
   if(result < 0) {
     throwErrNoOnSysCallException(_T("_filelengthi64"));
   }
--- Lib/Src/Util/fseek.cpp
+++ Lib/Src/Util/fseek.cpp
@@ -1,8 +1,13 @@
 #include "pch.h"
 #include <MyUtil.h>
 
-void FSEEK(FILE *f, __int64 offset) {
+void FSEEK(FILE *f, INT64 offset) {
   if(fsetpos(f, &offset) != 0 ) {
-    throwException(_T("FSEEK:fsetpos(%I64d) failed:%s"), offset, getErrnoText().cstr());
+    throwException(_T("%s:fsetpos(%I64d) failed:%s. FileSize=%I64d")
+                  ,__TFUNCTION__
+                  ,offset
+                  ,getErrnoText().cstr()
+                  ,GETSIZE(f)
+                  );
   }
 }
