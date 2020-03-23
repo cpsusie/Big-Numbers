3d422d4c5c9aefb2d5596a2fc2706a4b5d440eac
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-03-23 20:02:56 +0100

    Fixed bug in Singleton.cpp. (No more need for #pragma init_seg(lib))

--- Lib/Src/Util/Singleton.cpp
+++ Lib/Src/Util/Singleton.cpp
@@ -2,33 +2,65 @@
 #include <cstdlib>
 #include <Singleton.h>
 
-#pragma warning(disable : 4073)
-#pragma init_seg(lib)
+typedef enum {
+  REQUEST_STACK
+ ,RELEASE_STACK
+} StackRequest;
 
-static FastSemaphore            lock;
-static CompactStack<Singleton*> singletonStack;
-static bool                     destructorInstalled = false;
+typedef CompactStack<Singleton*> _SingletonStack;
+
+static _SingletonStack *stackRequest(StackRequest request) {
+  static FastSemaphore    lock;
+  static _SingletonStack *stack = NULL;
+  switch(request) {
+  case REQUEST_STACK:
+    lock.wait();
+    if(stack == NULL) {
+      stack = new _SingletonStack();
+    }
+    return stack;
+  case RELEASE_STACK:
+    if(stack && stack->isEmpty()) {
+      delete stack;
+      stack = NULL;
+    }
+    lock.notify();
+    break;
+  default:
+    abort();
+  }
+  return NULL;
+}
+
+// Don't use debugLog in here. infinite recursion
+static inline _SingletonStack &getStack() {
+  return *stackRequest(REQUEST_STACK);
+}
+
+static inline void releaseStack() {
+  stackRequest(RELEASE_STACK);
+}
 
 // Don't use debugLog in here. infinite recursion
 void Singleton::registerThis() {
-  lock.wait();
   if(!m_registered) {
-    singletonStack.push(this);
+    getStack().push(this);
     m_registered = true;
-    if (!destructorInstalled) {
+    static bool destructorInstalled = false;
+    if(!destructorInstalled) {
       atexit(singletonsDestructor);
       destructorInstalled = true;
     }
+    releaseStack();
   }
-  lock.notify();
 }
 
 // Don't use debugLog in here. infinite recursion
 static void singletonsDestructor() {
-  lock.wait();
-  while(!singletonStack.isEmpty()) {
-    Singleton *s = singletonStack.pop();
+  _SingletonStack &stack = getStack();
+  while(!stack.isEmpty()) {
+    Singleton *s = stack.pop();
     s->m_factory.releaseInstance();
   }
-  lock.notify();
+  releaseStack();
 }
