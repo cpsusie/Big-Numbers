7357a7fbe27e5dd2d8bd2bbbe4265734a129c167
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-03-29 17:31:34 +0200

    Added 1st version of files GenericDebugger.cpp/.h, implements a base-class for a Debugger

---
+++ Lib/Include/GenericDebugger.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <InterruptableRunnable.h>
+#include <PropertyContainer.h>
+
+typedef enum {
+  DEBUGGER_STATE
+} DebuggerProperties;
+
+typedef enum {
+  DEBUGGER_CREATED
+ ,DEBUGGER_RUNNING
+ ,DEBUGGER_PAUSED
+ ,DEBUGGER_TERMINATED
+} DebuggerState;
+
+class GenericDebugger;
+
+class AbstractDebugableVisualizer : public PropertyChangeListener {
+public:
+  virtual bool isbreakPoint( GenericDebugger &debugger) = 0;
+  virtual void clearAllBreakPoints()                    = 0;
+};
+
+class AbstractDebugable : public Runnable {
+  friend class GenericDebugger;
+protected:
+  GenericDebugger *m_debugger;
+  virtual const void *getData() const = 0;
+};
+
+template<typename T> class Debugable : public AbstractDebugable {
+public:
+  inline const T &getState() const {
+    return *(T*)getData();
+  }
+};
+
+class GenericDebugger : public InterruptableRunnable, public PropertyContainer {
+private:
+  DebuggerState                m_state;
+  AbstractDebugable           &m_debugable;
+  AbstractDebugableVisualizer &m_visualizer;
+  bool                         m_breakPointsEnabled;
+  GenericDebugger &checkTerminated();
+  GenericDebugger &enableBreak(bool enable);
+  void suspend();
+public:
+  GenericDebugger(AbstractDebugable &debugable, AbstractDebugableVisualizer &visualizer);
+  ~GenericDebugger();
+  void singleStep();
+  void go();
+  void kill();
+  UINT safeRun();
+  void handleStep();
+  inline DebuggerState getState() const {
+    return m_state;
+  }
+  inline String getStateName() const {
+    return getStateName(getState());
+  }
+  static String getStateName(DebuggerState state);
+  const AbstractDebugable &getDebugable() const {
+    return m_debugable;
+  }
+};
---
+++ Lib/Src/Util/GenericDebugger.cpp
@@ -0,0 +1,84 @@
+#include "pch.h"
+#include <GenericDebugger.h>
+#include <Thread.h>
+
+GenericDebugger::GenericDebugger(AbstractDebugable &debugable, AbstractDebugableVisualizer &visualizer)
+: m_state(     DEBUGGER_CREATED)
+, m_debugable( debugable       )
+, m_visualizer(visualizer      )
+{
+  addPropertyChangeListener(&m_visualizer);
+  m_debugable.m_debugger = this;
+}
+
+GenericDebugger::~GenericDebugger() {
+  kill();
+  m_debugable.m_debugger = NULL;
+}
+
+void GenericDebugger::singleStep() {
+  checkTerminated().enableBreak(false).resume();
+}
+
+void GenericDebugger::go() {
+  m_visualizer.clearAllBreakPoints();
+  checkTerminated().enableBreak(false).resume();
+}
+
+void GenericDebugger::kill() {
+  setInterrupted();
+  waitUntilJobDone();
+  setProperty(DEBUGGER_STATE, m_state, DEBUGGER_TERMINATED);
+}
+
+UINT GenericDebugger::safeRun() {
+  SETTHREADDESCRIPTION("Debugger");
+  setProperty(DEBUGGER_STATE, m_state, DEBUGGER_RUNNING);
+  m_debugable.run();
+  setProperty(DEBUGGER_STATE, m_state, DEBUGGER_TERMINATED);
+  return 0;
+}
+
+void GenericDebugger::suspend() {
+  setProperty(DEBUGGER_STATE, m_state, DEBUGGER_PAUSED);
+  __super::suspend();
+  setProperty(DEBUGGER_STATE, m_state, DEBUGGER_RUNNING);
+}
+
+void GenericDebugger::handleStep() {
+  if(isInterruptedOrSuspended()) {
+    if(isInterrupted()) {
+      die();
+    } else if(isSuspended()) {
+      suspend();
+      return;
+    }
+  }
+  if(m_breakPointsEnabled && m_visualizer.isbreakPoint(*this)) {
+    suspend();
+  }
+}
+
+String GenericDebugger::getStateName(DebuggerState state) { // static
+#define CASESTR(s) case DEBUGGER_##s: return _T(#s)
+  switch (state) {
+  CASESTR(CREATED   );
+  CASESTR(RUNNING   );
+  CASESTR(PAUSED    );
+  CASESTR(TERMINATED);
+  default: return format(_T("Unknown debuggerState:%d"), state);
+  }
+#undef CASESTR
+}
+
+GenericDebugger &GenericDebugger::checkTerminated() {
+  if(getState() == DEBUGGER_TERMINATED) {
+    throwException(_T("Debugger is terminated"));
+  }
+  return *this;
+}
+
+GenericDebugger &GenericDebugger::enableBreak(bool enable) {
+  m_breakPointsEnabled = enable;
+  return *this;
+}
