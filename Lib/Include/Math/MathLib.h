8df70e5591451b7adfebf47fc1f84aaa7362dc36
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-05-11 23:57:22 +0200

    Added template FunctionWithTimeTemplate

--- Lib/Include/Math/MathLib.h
+++ Lib/Include/Math/MathLib.h
@@ -12,3 +12,10 @@ public:
 typedef FunctionTemplate<Real,Real>         Function;
 
 CompactRealArray findZeroes(  Function &f, const RealInterval &interval);
+
+template <typename F> class FunctionWithTimeTemplate : public F {
+public:
+  virtual void        setTime(const Real &time)   = 0;
+  virtual const Real &getTime() const             = 0;
+  virtual FunctionWithTimeTemplate *clone() const = 0;
+};
