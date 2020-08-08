13e69dbd61dfac5a6d9a5ce399c48c3ec57fe263
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-08-08 17:50:07 +0200

    Removed 4 old projects

--- D3FunctionPlotter/ProfilePainter.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <MFCUtil/Viewport2D.h>
-
-void paintProfile(const Profile &profile, Viewport2D &vp, COLORREF color);
-void paintProfilePolygon(const ProfilePolygon &pp, Viewport2D &vp, COLORREF color);
-void paintProfileNormals(const Profile &profile, Viewport2D &vp, COLORREF color, bool smooth);
--- Timtalk/StdAfx.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "targetver.h"
-#include <MFCUtil/WinTools.h>
-#include "Timtalk.h"
