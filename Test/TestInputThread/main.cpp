5d78b5f700789d85a84ebeda5d3c182b492877c5
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-09-11 22:05:23 +0200

    some experiments with TestInputThread

--- Test/TestInputThread/main.cpp
+++ Test/TestInputThread/main.cpp
@@ -5,21 +5,19 @@ int main(int argc, char **argv) {
   InputThread input(stdin);
   int timeout = 5000;
 
-  while (!input.endOfInput()) {
+  while(!input.endOfInput()) {
     try {
       const String line = input.getLine(timeout);
-      if (line == _T("quit")) break;
-      if (line == _T("kill")) input.kill();
+      if(line == _T("quit")) break;
+      if(line == _T("kill")) input.kill();
       int newTimeout;
-      if (_stscanf(line.cstr(), _T("timeout=%d"), &newTimeout) == 1) {
+      if(_stscanf(line.cstr(), _T("timeout=%d"), &newTimeout) == 1) {
         timeout = newTimeout;
       }
       _tprintf(_T("read line \"%s\"\n"), line.cstr());
-    }
-    catch (TimeoutException e) {
+    } catch(TimeoutException e) {
       _tprintf(_T("Timeout:%s\n"), e.what());
-    }
-    catch (Exception e) {
+    } catch(Exception e) {
       _tprintf(_T("Exception:%s\n"), e.what());
     }
   }
