adbfa38cc2e3b8ef706a3ac8bbd3e398f741baf3
Jesper <jesper.gr.mikkelsen@gmail.com>
2020-12-20 21:50:25 +0100

    Added copyright note

--- Lib/Src/Math/Int128x64.asm
+++ Lib/Src/Math/Int128x64.asm
@@ -1,3 +1,26 @@
+;
+; Copyright (C) 2020 by Jesper G. Mikkelsen
+;
+; https://github.com/JesperMikkelsen/Big-Numbers/tree/master/Lib/Src/Math
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy of this
+; software and associated documentation files (the "Software"), to deal in the Software
+; without restriction, including without l> imitation the rights to use, copy, modify, merge,
+; publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to
+; whom the Software is furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all copies or
+; substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+; IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+; IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+;
 ; These functions implements the basic operations for _int128 type
 ; running on 64-bit intel CPU.
 ; They are (almost) identical to MS's workhorse-functions that do
